3aa82fee2a9075f5dc79e586c4e53f947b2e820c
prathamesh <shendeprathamesh66@gmail.com>
2022-10-28 12:02:11 +0530

    normal commit

---
+++ src/main/java/com/training/ProductAppFinal4Application.java
@@ -0,0 +1,15 @@
+package com.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductAppFinal4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductAppFinal4Application.class, args);
+		
+		System.out.println("Application Started");
+	}
+
+}
